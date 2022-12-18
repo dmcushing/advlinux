ab5adc5d0829e759cb0eaaf5a2510eebf0aa57f7
Dave Cushing <dave@davecushing.ca>
2022-12-18 16:04:00 -0500

    Update functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -44,7 +44,7 @@ echo -e "  Student number: $snumber"
 echo -e "   Email address: $mailaddy"
 blank_line
 echo -e "Instructor email: $inmailaddy"
-read -n 1 -r -s -p $'Press any key to continue or CTRL-C to exit\n'
+read -n 1 -r -s -p $'Press enter to continue...\n'
 
 #if [ "$key" = ' ' ]; then
     # Space pressed, do something
