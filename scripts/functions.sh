e908ae1dd601135c04bbf962cf98aeb0bcaead9c
Dave Cushing <dave@davecushing.ca>
2022-12-19 07:04:26 -0500

    Update functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -111,6 +111,9 @@ content=$( base64 -w0 $outfile )
 attachment="$1_$2-$lname-$fname.txt"
 
 read -p "Mail your work to your instructor? (y to send mail or CTRL-C to exit) "
+
+echo "$( cat $outfile ) | mailx -s "$lname $fname: $1 $2 " -r mailrelay@cety.online $mailaddy,$inmailaddy
+
 # [ "${REPLY,,}" != "y" ] || curl --request POST \
 #  --url https://api.sendgrid.com/v3/mail/send \
 #  --header "Authorization: Bearer $SENDGRID_API_KEY" \
