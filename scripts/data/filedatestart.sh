4402c42ab7a57cbb7ee946ebdd33ececfeb9e623
Dave Cushing <dave@davecushing.ca>
2023-01-18 15:04:17 -0500

    Create filedatestart.sh

---
+++ scripts/data/filedatestart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+#        Name: Your Name and Student Number
+# Description: Create a directory and filename in user’s home directory
+#              appends date and time to filename
+#              inserts today’s date in filename 
+#              outputs contents of filename when done
+#              
+#       Usage: filedate.sh <directory> <filename>
+# Variables
+DIRNAME=
+FILENAME=
+
+
+#
+# Create the directory using mkdir ~/DIRNAME
+# Create the file using date command and redirect output to FILENAME
+#
+
+#
+# Output the contents of the file using cat
+#
