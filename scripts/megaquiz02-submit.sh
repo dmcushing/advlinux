c3fad34894e1fe56c50f3650fae26e04f8783df7
Dave Cushing <dave@davecushing.ca>
2023-01-31 05:39:28 -0500

    Update megaquiz02-submit.sh

--- scripts/megaquiz02-submit.sh
+++ scripts/megaquiz02-submit.sh
@@ -12,17 +12,17 @@ clear
 is_super_user
 
 # Gather student information
-student_info IPAddr Quiz02 CET1034
+student_info MegaCorp Quiz02 CET1034
 
 echo -e "*****" | tee -a $outfile
 echo -e "**      tree ~/quiz02/" | tee -a $outfile
 echo -e "*****" | tee -a $outfile
-tree ~/quiz02/ | tee -a $outfile
+tree -p ~/quiz02/ | tee -a $outfile
 blank_line
 echo -e "*****" | tee -a $outfile
-echo -e "**      ~/quiz02/ipaddr.sh" | tee -a $outfile
+echo -e "**      ~/quiz02/createuser.sh" | tee -a $outfile
 echo -e "*****" | tee -a $outfile
-nl -ba -nrz -w3 ~/quiz02/ipaddr.sh | tee -a $outfile
+nl -ba -nrz -w3 ~/quiz02/createuser.sh | tee -a $outfile
 
 # Mail information to instructor and student
-mail_out IPAddr Quiz02 CET1034
+mail_out MegaCorp Quiz02 CET1034
