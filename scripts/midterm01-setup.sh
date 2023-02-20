9963e37e14078283184853ef9132fe4d8dc3b243
Dave Cushing <dave@davecushing.ca>
2023-02-20 08:11:50 -0500

    Update midterm01-setup.sh

--- scripts/midterm01-setup.sh
+++ scripts/midterm01-setup.sh
@@ -17,7 +17,7 @@ rm -Rf /var/www/*
 mkdir -p /var/www/html
 cp /scripts/data/midterm01.html /var/www/html/index.html
 chown -R linuxuser:linuxuser /var/www/html
-rm ~/.ssh/id_rsa*
+rm ~/.ssh/id_rsa*  >/dev/null 2>&1
 groupadd midterm >/dev/null 2>&1
 mkdir ~/midterm >/dev/null 2>&1
 chown linuxuser:linuxuser ~/midterm
