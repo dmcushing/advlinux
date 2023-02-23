f4a529f5a16a0333103a27f8fc694ab7216d3c38
Dave Cushing <dave@davecushing.ca>
2023-02-23 05:20:15 -0500

    midterm2

---
+++ scripts/data/midterm-makepage02.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "<html><head><title>CET1034 MidTerm 02 Page</title></head><body>"
+echo "<h1>** Looping and CSV File **</h1>"
+echo "<h1>IP Address: </h1>"
+echo "<h1>Your Name Here</h1>"
+# Write your loop here
+
+echo "</body></html>"
