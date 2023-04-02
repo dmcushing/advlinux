05051d938680704ed301facdf782871d18875b0a
Dave Cushing <dave@davecushing.ca>
2023-04-02 15:13:24 -0400

    Update docker-cleanup.sh

--- scripts/docker-cleanup.sh
+++ scripts/docker-cleanup.sh
@@ -7,7 +7,7 @@ clear
 is_super_user
 
 echo -e "Starting.."
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+docker kill $(docker ps -q) >/dev/null 2>&1
+docker rm $(docker ps -a -q) >/dev/null 2>&1
+docker rmi $(docker images -q) >/dev/null 2>&1
 echo -e "..Complete"
