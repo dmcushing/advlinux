c098f36e608e70e24b7dd9a95933323b7facb13a
Dave Cushing <dave@davecushing.ca>
2023-04-02 14:48:23 -0400

    quiz

---
+++ scripts/docker-cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /scripts/functions.sh
+clear
+
+# Verify script is running as root
+is_super_user
+
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
