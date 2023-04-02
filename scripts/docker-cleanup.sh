7d72045b08d0ebb57fc6ed48fed1aaea436f6a20
Dave Cushing <dave@davecushing.ca>
2023-04-02 15:02:21 -0400

    Update docker-cleanup.sh

--- scripts/docker-cleanup.sh
+++ scripts/docker-cleanup.sh
@@ -6,6 +6,6 @@ clear
 # Verify script is running as root
 is_super_user
 
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+docker kill $(docker ps -q) &2>/dev/null
+docker rm $(docker ps -a -q) &2>/dev/null
+docker rmi $(docker images -q) &2>/dev/null
