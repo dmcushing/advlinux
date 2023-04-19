be9ac4d0ea2467af524a31f48110433207164e9c
Dave Cushing <dave@davecushing.ca>
2023-04-19 15:48:48 -0400

    final

---
+++ scripts/data/final-makepage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DATAFILE="/scripts/data/final.csv"
+
+# LOOP
+OUTPUTFILE=
+
+echo "<html><head><title>CET1034 Final</title></head><body>"
+echo "<h1>Final - YOUR-NAME-HERE - A000000</h1>"
+echo "<h1>IP Address: IPADDRESS-HERE</h1>"
+echo "<table><tr><td><img src='https://picsum.photos/100'></td>"
+echo "<td>Username: USER-HERE<br>IP: USER_IP-HERE<br>Logon: USER_LOGON-HERE<br></td></tr></table>"
+echo "USER_EXPIRY-HERE"
+echo "</body></html>"
+
+# END LOOP
