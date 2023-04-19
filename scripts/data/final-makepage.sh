3a5391a2462b5a81eb7330d9c174d0bbf1127af7
Dave Cushing <dave@davecushing.ca>
2023-04-19 16:40:02 -0400

    Update final-makepage.sh

--- scripts/data/final-makepage.sh
+++ scripts/data/final-makepage.sh
@@ -4,13 +4,15 @@ DATAFILE="/scripts/data/final.csv"
 
 # LOOP
 OUTPUTFILE=
+USER_EXPIRY=
+IPADDRESS=
 
 echo "<html><head><title>CET1034 Final</title></head><body>"
 echo "<h1>Final - YOUR-NAME-HERE - A000000</h1>"
 echo "<h1>IP Address: IPADDRESS-HERE</h1>"
 echo "<table><tr><td><img src='https://picsum.photos/100'></td>"
-echo "<td>Username: USER-HERE<br>IP: USER_IP-HERE<br>Logon: USER_LOGON-HERE<br></td></tr></table>"
-echo "USER_EXPIRY-HERE"
+echo "<td>Username: USER_ID-HERE<br>IP: USER_IP-HERE<br>Logon: USER_LOGON-HERE<br></td></tr></table>"
+echo "Expiry: USER_EXPIRY-HERE"
 echo "</body></html>"
 
 # END LOOP
