5e0d9edc6e8b06bf3860507e2d01484d8c074438
Dave Cushing <dave@davecushing.ca>
2023-04-30 13:22:30 -0400

    final2

---
+++ scripts/data/final-pagemake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DATAFILE="/scripts/data/final-pagemake.csv"
+
+# BEGIN LOOP
+
+OUTPUTFILE=
+USER_EXPIRY=
+IPADDRESS=
+
+echo "<html><head><title>CET1034 Final</title></head><body>"
+echo "<h1>Final - YOUR-NAME-HERE - A000000</h1>"
+echo "<h1>IP Address: IPADDRESS-HERE</h1>"
+echo "<table><tr><td>Name: USER_FIRST_NAME-HERE USER_LAST_NAME-HERE<br>EMAIL: USER_EMAIL-HERE<br>Hire Date: USER_HIRE_DATE-HERE<br></td>"
+echo "<td><img src='https://picsum.photos/100'></td></tr></table>"
+echo "Expiry: USER_EXPIRY-HERE"
+echo "</body></html>"
+
+# END LOOP
